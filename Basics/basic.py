8985b857ed08a98bc846c40ca95a38a8a4f247cd
anjalaN <anjala2022@gmail.com>
2026-09-10 19:54:26 +0200

    basic python

--- Basics/basic.py
+++ Basics/basic.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-data={
-    "name":["anju", "sara", "john"],
-    "age":[25, 30, 28]
+data = {
+    "name": ["anju", "sara", "john"],
+    "age": [25, 30, 28]
 }
-df = pd.DataFrame(data)
-print(df)
+pd.DataFrame()
