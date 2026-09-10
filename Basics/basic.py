d1e640cf3565ff34db7373315768e7e02d69d66a
anjalaN <anjala2022@gmail.com>
2026-09-10 20:19:35 +0200

    basic python

--- Basics/basic.py
+++ Basics/basic.py
@@ -4,5 +4,11 @@ data = {
     "name": ["anju", "sara", "john"],
     "age": [25, 30, 28]
 }
-pd.DataFrame()
-print(pd.DataFrame())
+
+df = pd.DataFrame(data)
+df["age"].mean()
+df["age"].median()
+
+print(df)
+print(df["age"].mean())
+print(df["age"].median())
