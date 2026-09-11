6c77a6903f0278d4ddc301db9cee24ebd6020c05
anjalaN <anjala2022@gmail.com>
2026-09-11 21:41:31 +0200

    dataframe

---
+++ Dataframe/datafram.py
@@ -0,0 +1,13 @@
+import pandas as pd
+data = {
+    "Name":["anju", "marie", "john"],
+    "age":[25, 30, 28],
+    "city": ["paris", "lyon", "paris"]
+
+}
+df = pd.DataFrame(data)
+df.to_csv("people.csv", index=False)
+df.to_excel("people.xlsx", index=False)
+print(df["Name"])
+print(df)
+
